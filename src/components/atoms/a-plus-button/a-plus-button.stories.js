ea4401d59f357203fdf486202e48ff182a6903f6
mpq <m3atyy@gmail.com>
2022-02-14 18:51:48 +0500

    plus-button wip

---
+++ src/components/atoms/a-plus-button/a-plus-button.stories.js
@@ -0,0 +1,19 @@
+import APlusButton from './a-plus-button';
+
+export default {
+    title: 'ui system/atoms/a-plus-button',
+    components: APlusButton,
+    args: {
+        big: false,
+        disabled: false,
+    },
+    template: '<a-plus-button v-bind="args"/>',
+};
+
+export const PlusButton = (args) => ({
+    components: { APlusButton },
+    setup() {
+        return { args };
+    },
+    template: '<a-plus-button v-bind="args"/>',
+});
---
+++ src/components/atoms/a-plus-button/a-plus-button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button
+        class="a-plus-button"
+        :disabled="disabled"
+    >
+    </button>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'a-plus-button',
+    props: {
+        big: Boolean,
+        disabled: Boolean,
+    },
+});
+</script>
+
+<style src="./a-plus-button.css">
+</style>
