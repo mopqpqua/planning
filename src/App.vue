c5451043d17d7cfeb05c03e89797db63d052ce43
mpq <m3atyy@gmail.com>
2022-03-06 14:26:22 +0500

    option

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
     <main>
-        <MOption>
-            <template #image>
-                <img src="@assets/icons/icon-plus.svg"/>
-            </template>
-        </MOption>
     </main>
 </template>
 
 <script>
-import MOption from '@molecules/m-option/m-option';
-
 export default {
     name: 'App',
-    components: { MOption },
 };
 </script>
--- src/components/molecules/m-option/m-option.stories.js
+++ src/components/molecules/m-option/m-option.stories.js
@@ -31,8 +31,8 @@ ObjectOption.args = {
         name: 'Rick',
         surname: 'Sanchez',
     },
-}
-;
+};
+
 export const IconedOption = (args) => ({
     components: { MOption },
     setup() {
@@ -41,8 +41,23 @@ export const IconedOption = (args) => ({
     template: `
         <m-option v-bind="args">
             <template #image>
-                <img src="assets/icon-plus.svg"/>
+                <img class="m-option__icon" src="assets/icon-study-hat.svg"/>
             </template>
         </m-option>
     `,
 });
+
+export const ActiveIconedOption = (args) => ({
+    components: { MOption },
+    setup() {
+        return { args };
+    },
+    template: `
+        <m-option v-bind="args">
+            <template #image>
+                <img class="m-option__icon" src="assets/icon-study-hat-active.svg"/>
+            </template>
+        </m-option>
+    `,
+});
+ActiveIconedOption.args = {...ActiveIconedOption.args, active: true};
